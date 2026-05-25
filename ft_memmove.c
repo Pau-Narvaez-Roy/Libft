d81c70a48543936b0003dc7cd92be756815ae89b
ptitchat <pau.roy.narvaez@gmail.com>
2026-05-25 16:46:52 +0200

    Copia los bytes de un puntero a otro evitando el overlap

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: pnarvaez <pnarvaez@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/25 15:09:20 by pnarvaez          #+#    #+#             */
-/*   Updated: 2026/05/25 16:11:07 by pnarvaez         ###   ########.fr       */
+/*   Updated: 2026/05/25 16:42:08 by pnarvaez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,19 +33,3 @@ void	*ft_memmove(void *dest, const void *src, size_t n)
 	}
 	return (dest);
 }
-
-/*int	main(int argc, char **argv)
-{
-	#include <stdio.h>
-	#include <string.h>
-
-	//char src[] = "test basic du memcpy !";
-	char	buff1[22];
-	char	buff2[22];
-
-	memmove(buff1, argv[1], 22);
-	ft_memmove(buff2, argv[1], 22);
-	printf("1: %s\n", buff1);
-	printf("2: %s\n", buff2);
-	return (0);
-}*/
