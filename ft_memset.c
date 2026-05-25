d3f023fe508bec2526cb0ba0f0d0399a527e973a
ptitchat <pau.roy.narvaez@gmail.com>
2026-05-25 16:44:59 +0200

    Remplaza los valores de un puntero por una constante

--- ft_memset.c
+++ ft_memset.c
@@ -6,7 +6,7 @@
 /*   By: pnarvaez <pnarvaez@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/25 11:09:18 by pnarvaez          #+#    #+#             */
-/*   Updated: 2026/05/25 11:42:16 by pnarvaez         ###   ########.fr       */
+/*   Updated: 2026/05/25 16:41:38 by pnarvaez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,13 +21,3 @@ void	*ft_memset(void *s, int c, size_t n)
 		*p++ = (unsigned char) c;
 	return (s);
 }
-
-/*int	main(int argc, char **argv)
-{
-	#include <stdio.h>
-
-	printf("%s\n", argv[1]);
-	ft_memset(argv[1], 45, 5);
-	printf("%s\n", argv[1]);
-	return (0);
-}*/
