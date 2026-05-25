1f33fd89d8b2987823d7205bfb0a173bd2d5023b
ptitchat <pau.roy.narvaez@gmail.com>
2026-05-25 16:44:22 +0200

    Copia los valores en memoria de un puntero a otro

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: pnarvaez <pnarvaez@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/25 12:13:23 by pnarvaez          #+#    #+#             */
-/*   Updated: 2026/05/25 15:05:54 by pnarvaez         ###   ########.fr       */
+/*   Updated: 2026/05/25 16:42:50 by pnarvaez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,19 +25,3 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 		*to++ = *from++;
 	return (dest);
 }
-
-/*int	main(int argc, char **argv)
-{
-	#include <stdio.h>
-	#include <string.h>
-
-	//char src[] = "test basic du memcpy !";
-	char	buff1[22];
-	char	buff2[22];
-
-	//memcpy(buff1, argv[1], 22);
-	ft_memcpy(buff2, argv[1], 22);
-	printf("1: %s\n", buff1);
-	printf("2: %s\n", buff2);
-	return (0);
-}*/
