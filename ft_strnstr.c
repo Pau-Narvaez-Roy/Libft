288e93609fa92c882ef7a7f8bb2a079ce530d3f2
ptitchat <pau.roy.narvaez@gmail.com>
2026-05-27 17:11:48 +0200

    Busca un string en otro

---
+++ ft_strnstr.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pnarvaez <pnarvaez@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/27 16:14:04 by pnarvaez          #+#    #+#             */
+/*   Updated: 2026/05/27 17:11:14 by pnarvaez         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strnstr(const char *big, const char *little, size_t len)
+{
+	char		*str;
+	char		*to_find;
+	size_t		i;
+	size_t		j;
+	size_t		max;
+
+	str = (char *) big;
+	to_find = (char *) little;
+	i = 0;
+	max = len;
+	if (*to_find == '\0')
+		return (str);
+	while (str[i] && len)
+	{
+		j = 0;
+		while (str[i + j] == to_find[j] && to_find[j] && (i + j) < max)
+		{
+			j++;
+			if (to_find[j] == '\0')
+				return (str + i);
+		}
+		i++;
+		len--;
+	}
+	return (NULL);
+}
