2198e3251578811e0d22b8caec70153e0e75164a
ptitchat <pau.roy.narvaez@gmail.com>
2026-05-27 17:20:09 +0200

    Makefile y libft.h

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: pnarvaez <pnarvaez@student.42madrid.com    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2026/05/19 16:26:53 by pnarvaez          #+#    #+#              #
-#    Updated: 2026/05/25 17:09:19 by pnarvaez         ###   ########.fr        #
+#    Updated: 2026/05/27 17:15:09 by pnarvaez         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -14,7 +14,9 @@ NAME = libft.a
 
 SOURCES = ft_isalpha.c ft_isdigit.c ft_isalnum.c ft_isascii.c ft_isprint.c\
 		  ft_strlen.c ft_memset.c ft_bzero.c ft_memcpy.c ft_memmove.c\
-		  ft_strlcpy.c ft_strlcat\
+		  ft_strlcpy.c ft_strlcat.c ft_toupper.c ft_tolower.c ft_strchr.c\
+		  ft_strrchr.c ft_strncmp.c ft_memchr.c ft_memcmp.c ft_strnstr.c\
+		  ft_atoi.c\
 
 OBJECTS = $(SOURCES:.c=.o)
 
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: pnarvaez <pnarvaez@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/25 09:41:25 by pnarvaez          #+#    #+#             */
-/*   Updated: 2026/05/25 17:09:08 by pnarvaez         ###   ########.fr       */
+/*   Updated: 2026/05/27 17:14:41 by pnarvaez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,5 +27,14 @@ void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
 size_t	ft_strlcpy(char *dst, const char *src, size_t size);
 size_t	ft_strlcat(char *dst, const char *src, size_t size);
+int		ft_toupper(int c);
+int		ft_tolower(int c);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strrchr(const char *s, int c);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
+void	*ft_memchr(const void *s, int c, size_t n);
+int		ft_memcmp(const void *s1, const void *s2, size_t n);
+char	*ft_strnstr(const char *big, const char *little, size_t len);
+int		ft_atoi(const char *nptr);
 
 #endif
