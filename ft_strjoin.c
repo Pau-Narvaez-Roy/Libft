9fbb8eb3615baf4b95e76ec842865c1cd2cc9bbe
Ptitchat <pau.roy.narvaez@gmail.com>
2026-06-01 15:33:22 +0200

    Reserva memory devuelve una nueva cadena de caracteres, formada por la concatenación de ‘s1’ y ‘s2’

---
+++ ft_strjoin.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pnarvaez <pnarvaez@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/06/01 15:15:12 by pnarvaez          #+#    #+#             */
+/*   Updated: 2026/06/01 15:33:11 by pnarvaez         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char			*s;
+	unsigned int	i;
+
+	s = (char *) malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
+	if (!s)
+		return (NULL);
+	i = 0;
+	while (*s1)
+		s[i++] = *s1++;
+	while (*s2)
+		s[i++] = *s2++;
+	s[i] = '\0';
+	return (s);
+}
