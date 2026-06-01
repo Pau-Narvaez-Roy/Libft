04e320d8f4f2c44125d727c5e0000f42f2e9650f
Ptitchat <pau.roy.narvaez@gmail.com>
2026-06-01 18:35:01 +0200

    Reserva memory devuelve una copia de ‘s1’ con los caracteres de ‘set’ eliminados al principio y al final.

---
+++ ft_strtrim.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strtrim.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pnarvaez <pnarvaez@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/06/01 17:53:48 by pnarvaez          #+#    #+#             */
+/*   Updated: 2026/06/01 18:33:42 by pnarvaez         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strtrim(char const *s1, char const *set)
+{
+	size_t			len;
+
+	if (!s1 || !set)
+		return (NULL);
+	while (*s1 && ft_strchr(set, *s1))
+		s1++;
+	len = ft_strlen(s1);
+	while (ft_strchr(set, s1[len]) && len)
+		len--;
+	return (ft_substr(s1, 0, len + 1));
+}
