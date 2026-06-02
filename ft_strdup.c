4a4062e5afcf4cff652d433e49d2eafa33b430f1
Ptitchat <pau.roy.narvaez@gmail.com>
2026-06-02 12:26:46 +0200

    Devuelve un puntero a al string copiado de src

---
+++ ft_strdup.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pnarvaez <pnarvaez@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/06/02 12:16:11 by pnarvaez          #+#    #+#             */
+/*   Updated: 2026/06/02 12:21:06 by pnarvaez         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strdup(char *src)
+{
+	char	*copy;
+	int		i;
+
+	i = 0;
+	copy = malloc(sizeof(char) * ft_strlen(src) + 1);
+	if (!copy)
+		return (NULL);
+	while (*src)
+		copy[i++] = *src++;
+	copy[i] = '\0';
+	return (copy);
+}
