65feffb0148c2a9a3f4d1ca206040e4da3d671d5
Ptitchat <pau.roy.narvaez@gmail.com>
2026-06-04 15:34:12 +0200

    Añade el nodo ‘new’ al final de la lista ‘lst’.

---
+++ ft_lstadd_back.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstadd_back.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pnarvaez <pnarvaez@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/06/04 15:17:12 by pnarvaez          #+#    #+#             */
+/*   Updated: 2026/06/04 15:33:30 by pnarvaez         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstadd_back(t_list **lst, t_list *new)
+{
+	t_list	*list;
+
+	list = ft_lstlast(*lst);
+	if (!list)
+		*lst = new;
+	else
+		list->next = new;
+}
