76dd0905d01e39797cec07b1502072fa4a3d362c
Ptitchat <pau.roy.narvaez@gmail.com>
2026-06-04 15:16:35 +0200

    Devuelve el último nodo de la lista.

---
+++ ft_lstlast.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstlast.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pnarvaez <pnarvaez@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/06/04 15:12:24 by pnarvaez          #+#    #+#             */
+/*   Updated: 2026/06/04 15:15:52 by pnarvaez         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstlast(t_list *lst)
+{
+	if (!lst)
+		return (NULL);
+	while (lst->next)
+		lst = lst->next;
+	return (lst);
+}
