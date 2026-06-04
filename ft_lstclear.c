7845a9676b348e8d68aad0f20a45df1e2387c9e4
Ptitchat <pau.roy.narvaez@gmail.com>
2026-06-04 15:55:22 +0200

    Elimina y libera el nodo ‘lst’ dado y todos los consecutivos del mismo, utilizando la función ‘del’ y free(3). Al final, el puntero a la lista debe ser NULL.

---
+++ ft_lstclear.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstclear.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pnarvaez <pnarvaez@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/06/04 15:38:57 by pnarvaez          #+#    #+#             */
+/*   Updated: 2026/06/04 15:54:23 by pnarvaez         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstclear(t_list **lst, void (*del)(void *))
+{
+	t_list	*list;
+	t_list	*temp;
+
+	list = *lst;
+	while (list)
+	{
+		temp = list->next;
+		ft_lstdelone(list, del);
+		list = temp;
+	}
+	*lst = NULL;
+}
