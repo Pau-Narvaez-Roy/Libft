2d68c7d512b6afd9d40ab68ad95715b64c4bbb3b
Ptitchat <pau.roy.narvaez@gmail.com>
2026-06-05 11:05:08 +0200

    Aplica la función ‘f’ a cada carácter de la string ‘s’, pasando como parámetros el índice de cada carácter dentro de ‘s’ y la dirección del propio carácter, que puede modificarse si es necesario.

--- ft_striteri.c
+++ ft_striteri.c
@@ -3,14 +3,12 @@
 /*                                                        :::      ::::::::   */
 /*   ft_striteri.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: pnarvaez <pnarvaez@student.42.fr>          +#+  +:+       +#+        */
+/*   By: pnarvaez <pnarvaez@student.42.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/02 17:19:48 by pnarvaez          #+#    #+#             */
-/*   Updated: 2026/06/02 17:36:55 by pnarvaez         ###   ########.fr       */
+/*   Updated: 2026/06/05 11:04:38 by pnarvaez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-#include <stdio.h>
 
 void	ft_striteri(char *s, void (*f)(unsigned int, char *))
 {
