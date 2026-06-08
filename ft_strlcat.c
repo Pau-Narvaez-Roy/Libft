33bcaef2eb26e4898c3989f706f63063ceebe381
Ptitchat <pau.roy.narvaez@gmail.com>
2026-06-08 12:44:30 +0200

    Copia n char de un string al final de otro

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: pnarvaez <pnarvaez@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/25 17:06:17 by pnarvaez          #+#    #+#             */
-/*   Updated: 2026/06/08 08:32:21 by pnarvaez         ###   ########.fr       */
+/*   Updated: 2026/06/08 12:43:30 by pnarvaez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,13 @@
 
 size_t	ft_strlcat(char *dst, const char *src, size_t size)
 {
-	size_t	dstlen;
-	size_t	srclen;
 	size_t	len;
 
-	dstlen = ft_strlen(dst);
-	srclen = ft_strlen(src);
-	len = dstlen;
-	if (size <= dstlen)
-		return (size + srclen);
+	len = ft_strlen(dst);
+	if (size <= len)
+		return (size + ft_strlen(src));
 	while ((size - len - 1) && *src)
 		dst[len++] = *src++;
 	dst[len] = '\0';
-	return (dstlen + srclen);
+	return (ft_strlen(dst) + ft_strlen(src));
 }
