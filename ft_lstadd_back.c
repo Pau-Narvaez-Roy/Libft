dddf73a93e54e4d0f87edcba73e8b1a85293001f
Ptitchat <pau.roy.narvaez@gmail.com>
2026-06-08 16:04:22 +0200

    Añade el nodo ‘new’ al final de la lista ‘lst’.

--- ft_lstadd_back.c
+++ ft_lstadd_back.c
@@ -6,7 +6,7 @@
 /*   By: pnarvaez <pnarvaez@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/04 15:17:12 by pnarvaez          #+#    #+#             */
-/*   Updated: 2026/06/04 15:33:30 by pnarvaez         ###   ########.fr       */
+/*   Updated: 2026/06/08 15:41:26 by pnarvaez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@ void	ft_lstadd_back(t_list **lst, t_list *new)
 {
 	t_list	*list;
 
+	if (!lst || !new)
+		return ;
 	list = ft_lstlast(*lst);
 	if (!list)
 		*lst = new;
